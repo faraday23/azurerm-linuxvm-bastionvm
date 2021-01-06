876aeed828a3879152965287078b9d6e77fde823
faraday23 <41235618+faraday23@users.noreply.github.com>
2021-01-06 14:39:50 -0500

    refactor

--- main.tf
+++ main.tf
@@ -1,31 +1,25 @@
-locals {
-  os_type = lower(var.settings.os_type)
-}
-
 resource "azurerm_public_ip" "bastion" {
-  name                = "${module.metadata.names.product_name}-bastion-public"
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
+  name                = "${var.names.product_name}-bastion-public"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags = var.tags
 
   allocation_method = "Static"
   sku               = "Basic"
-
-  tags = module.metadata.tags
 }
 
 resource "azurerm_network_interface" "bastion" {
-  name                = "${module.metadata.names.product_name}-bastion"
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
+  name                = "${var.names.product_name}-bastion"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags = var.tags
 
   ip_configuration {
     name                          = "bastion"
-    subnet_id                     = module.virtual_network.subnet["iaas-outbound-subnet"].id
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.bastion.id
   }
-
-  tags = module.metadata.tags
 }
 
 resource "azurerm_network_security_rule" "bastion_in" {
@@ -38,52 +32,39 @@ resource "azurerm_network_security_rule" "bastion_in" {
   destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = azurerm_network_interface.bastion.private_ip_address
-  resource_group_name         = module.resource_group.name
-  network_security_group_name = module.virtual_network.subnet_nsg_names["iaas-outbound-subnet"]
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.security_group_name
 }
 
 resource "azurerm_linux_virtual_machine" "bastion" {
-  for_each = local.os_type == "linux" ? var.settings.virtual_machine_settings : {}
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  tags                  = var.tags
+  name                  = try(var.settings.virtual_machine_settings.name, {})
+  size                  = try(var.settings.virtual_machine_settings.size, {})
+  admin_username        = try(var.settings.virtual_machine_settings.admin_username, {})
+  network_interface_ids = [azurerm_network_interface.bastion.id, ]
 
-  resource_group_name   = module.resource_group.name
-  location              = module.resource_group.location
-  size                  = each.value.size
-  admin_username        = each.value.admin_username
-  network_interface_ids = [ azurerm_network_interface.bastion.id, ]
-
-  allow_extension_operations      = try(each.value.allow_extension_operations, null)
-  computer_name                   = azurecaf_name.linux_computer_name[each.key].result
-  eviction_policy                 = try(each.value.eviction_policy, null)
-  max_bid_price                   = try(each.value.max_bid_price, null)
-  priority                        = try(each.value.priority, null)
-  provision_vm_agent              = try(each.value.provision_vm_agent, true)
-  zone                            = try(each.value.zone, null)
-  disable_password_authentication = try(each.value.disable_password_authentication, true)
-  custom_data                     = try(each.value.custom_data, null) == null ? null : filebase64(format("%s/%s", path.cwd, each.value.custom_data))
-  availability_set_id             = try(var.availability_sets[var.client_config.landingzone_key][each.value.availability_set_key].id, var.availability_sets[each.value.availability_sets].id, null)
-  proximity_placement_group_id    = try(var.proximity_placement_groups[var.client_config.landingzone_key][each.value.proximity_placement_group_key].id, var.proximity_placement_groups[each.value.proximity_placement_groups].id, null)
-
-  dynamic "admin_ssh_key" {
-    for_each = lookup(each.value, "disable_password_authentication", true) == true ? [1] : []
-
-    content {
-      username   = each.value.admin_username
-      public_key = tls_private_key.ssh[each.key].public_key_openssh
-    }
-  }
-
+  allow_extension_operations      = try(var.settings.virtual_machine_settings.allow_extension_operations, null)
+  computer_name                   = try(var.settings.virtual_machine_settings.name, null)
+  priority                        = try(var.settings.virtual_machine_settings.priority, null)
+  eviction_policy                 = try(var.settings.virtual_machine_settings.eviction_policy, null)
+  provision_vm_agent              = try(var.settings.virtual_machine_settings.provision_vm_agent, true)
+  zone                            = try(var.settings.virtual_machine_settings.zone, null)
+  disable_password_authentication = try(var.settings.virtual_machine_settings.disable_password_authentication, true)
+ 
   os_disk {
-    caching                   = try(each.value.os_disk.caching, null)
-    disk_size_gb              = try(each.value.os_disk.disk_size_gb, null)
-    name                      = try(azurecaf_name.os_disk_linux[each.key].result, null)
-    storage_account_type      = try(each.value.os_disk.storage_account_type, null)
-    write_accelerator_enabled = try(each.value.os_disk.write_accelerator_enabled, false)
+    caching                   = try(var.settings.virtual_machine_settings.os_disk.caching, null)
+    disk_size_gb              = try(var.settings.virtual_machine_settings.os_disk.disk_size_gb, null)
+    name                      = try(var.settings.virtual_machine_settings.os_disk_linux, null)
+    storage_account_type      = try(var.settings.virtual_machine_settings.os_disk.storage_account_type, null)
+    write_accelerator_enabled = try(var.settings.virtual_machine_settings.os_disk.write_accelerator_enabled, false)
   }
 
   source_image_reference {
-    publisher = try(each.value.source_image_reference.publisher, null)
-    offer     = try(each.value.source_image_reference.offer, null)
-    sku       = try(each.value.source_image_reference.sku, null)
-    version   = try(each.value.source_image_reference.version, null)
+    publisher = try(var.settings.virtual_machine_settings.source_image_reference.publisher, null)
+    offer     = try(var.settings.virtual_machine_settings.source_image_reference.offer, null)
+    sku       = try(var.settings.virtual_machine_settings.source_image_reference.sku, null)
+    version   = try(var.settings.virtual_machine_settings.source_image_reference.version, null)
   }
 }
